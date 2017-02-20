5dbab454650e9314f3fd6ef5fa8b05cc7070e012
Clay Herendeen <cherendeen@envative.com>
2017-02-20 16:54:55 -0500

    Initial commit : started customizing for MCI

--- routes/config.js
+++ routes/config.js
@@ -1,42 +1,43 @@
 {
-    "settings": {
-        "route_name": "kb",
-        "num_top_results": "10",
-        "date_format": "DD/MM/YYYY h:mmA",
-        "show_view_count": true,
-        "update_view_count_logged_in": false,
-        "show_published_date": true,
-        "sort_by": {
-            "field": "kb_viewcount",
-            "order": -1
-        },
-        "website_title": "openKB",
-        "show_featured_articles": true,
-        "show_featured_in_article": false,
-        "featured_articles_count": "4",
-        "password_protect": false,
-        "show_kb_meta": true,
-        "suggest_allowed": true,
-        "show_author_email": true,
-        "enable_mermaid_charts": false,
-        "app_context": "",
-        "links_blank_page": true,
-        "database": {
-            "type": "embedded"
-        },
-        "allow_voting": true,
-        "google_analytics": "",
-        "theme": "",
-        "show_logon": true,
-        "add_header_anchors": false,
-        "typeahead_search": true,
-        "index_article_body": false,
-        "enable_spellchecker": true,
-        "show_website_logo": true,
-        "website_description": "openKB is an Open Source Node.js Markdown based knowledge base/FAQ/Wiki app with powerful lunr search",
-        "article_versioning": false,
-        "api_auth_token": "",
-        "api_allowed": false,
-        "locale": "en"
-    }
+  "settings": {
+  "route_name": "kb",
+    "num_top_results": "10",
+    "date_format": "DD/MM/YYYY h:mmA",
+    "show_view_count": true,
+    "update_view_count_logged_in": false,
+    "show_published_date": true,
+    "sort_by": {
+    "field": "kb_viewcount",
+      "order": -1
+  },
+  "website_title": "MCI Wiki",
+    "show_featured_articles": true,
+    "show_featured_in_article": false,
+    "featured_articles_count": "4",
+    "password_protect": false,
+    "show_kb_meta": true,
+    "suggest_allowed": true,
+    "show_author_email": true,
+    "enable_mermaid_charts": false,
+    "app_context": "",
+    "links_blank_page": true,
+    "database": {
+    "type": "mongodb",
+      "connection_string": "mongodb://127.0.0.1:27017/openkb"
+  },
+  "allow_voting": true,
+    "google_analytics": "",
+    "theme": "twitter",
+    "show_logon": true,
+    "add_header_anchors": false,
+    "typeahead_search": true,
+    "index_article_body": false,
+    "enable_spellchecker": true,
+    "show_website_logo": true,
+    "website_description": "Wiki / Educational Section of MCI website",
+    "article_versioning": false,
+    "api_auth_token": "",
+    "api_allowed": false,
+    "locale": "en"
+}
 }
